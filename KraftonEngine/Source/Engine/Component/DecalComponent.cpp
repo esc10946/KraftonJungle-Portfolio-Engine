4f014ea4561c46bfee33ad480cb811649f2eca2b
조현석 <chohs990514@gmail.com>
2026-04-10 14:05:10 +0900

    Add: UDecalComponent

---
+++ KraftonEngine/Source/Engine/Component/DecalComponent.cpp
@@ -0,0 +1,43 @@
+﻿#include "DecalComponent.h"
+#include "Resource/ResourceManager.h"
+
+IMPLEMENT_CLASS(UDecalComponent, UPrimitiveComponent)
+
+void UDecalComponent::Serialize(FArchive& Ar)
+{
+}
+
+void UDecalComponent::PostDuplicate()
+{
+	UPrimitiveComponent::PostDuplicate();
+	SetTexture(TextureName);
+}
+
+void UDecalComponent::GetEditableProperties(TArray<FPropertyDescriptor>& OutProps)
+{
+}
+
+void UDecalComponent::PostEditProperty(const char* PropertyName)
+{
+}
+
+void UDecalComponent::UpdateLocalExtents()
+{
+	LocalExtents = DecalSize * 0.5f;
+}
+
+void UDecalComponent::SetTexture(const FName& InTextureName)
+{
+	TextureName = InTextureName;
+	CachedTexture = FResourceManager::Get().FindTexture(InTextureName);
+	// 텍스처 유무가 batcher/Primitive 경로 분기를 좌우하므로 Mesh 단계까지 재갱신 필요.
+	MarkProxyDirty(EDirtyFlag::Material);
+}
+
+void UDecalComponent::SetDecalSize(const FVector& InSize)
+{
+	DecalSize = InSize;
+	MarkRenderTransformDirty();
+	UpdateLocalExtents();
+	MarkWorldBoundsDirty();
+}
---
+++ KraftonEngine/Source/Engine/Component/DecalComponent.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "Component/PrimitiveComponent.h"
+#include "Core/ResourceTypes.h"
+#include "Object/FName.h"
+
+class UDecalComponent : public UPrimitiveComponent
+{
+public:
+	DECLARE_CLASS(UDecalComponent, UPrimitiveComponent)
+
+	UDecalComponent() = default;
+	~UDecalComponent() override = default;
+	void Serialize(FArchive& Ar) override;
+	void PostDuplicate() override;
+	void GetEditableProperties(TArray<FPropertyDescriptor>& OutProps) override;
+	void PostEditProperty(const char* PropertyName) override;
+	void UpdateLocalExtents();
+
+	void SetTexture(const FName& InTextureName);
+	void SetDecalSize(const FVector& InSize);
+
+private:
+
+	FTextureResource* CachedTexture = nullptr;
+	FName TextureName;
+	FVector DecalSize = { 100.0f, 100.0f, 100.0f };
+};
+
