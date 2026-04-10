2db7568634badb405246073d3e29ee52bf4026e6
조현석 <chohs990514@gmail.com>
2026-04-10 16:53:57 +0900

    Init DecalSceneProxy

---
+++ KraftonEngine/Source/Engine/Render/Proxy/DecalSceneProxy.cpp
@@ -0,0 +1,64 @@
+﻿#include "DecalSceneProxy.h"
+#include "Component/DecalComponent.h"
+#include "Render/Resource/MeshBufferManager.h"
+#include "Render/Resource/ShaderManager.h"
+
+FDecalSceneProxy::FDecalSceneProxy(UDecalComponent* InComponent)
+	: FPrimitiveSceneProxy(InComponent)
+{
+}
+
+void FDecalSceneProxy::UpdateTransform()
+{
+	UDecalComponent* Comp = GetDecalComponent();
+	if (!Comp) return;
+
+	PerObjectConstants = FPerObjectConstants::FromWorldMatrix(Comp->GetDecalToWorldMatrix());
+	CachedWorldPos = PerObjectConstants.Model.GetLocation();
+	CachedBounds = Comp->GetWorldBoundingBox();
+	LastLODUpdateFrame = UINT32_MAX;
+	MarkPerObjectCBDirty();
+}
+
+void FDecalSceneProxy::UpdateMesh()
+{
+	MeshBuffer = &FMeshBufferManager::Get().GetMeshBuffer(EMeshShape::Cube);
+	Shader = FShaderManager::Get().GetShader(EShaderType::Primitive);
+	Pass = ERenderPass::Decal;
+	RebuildSectionDraw();
+}
+
+void FDecalSceneProxy::UpdateMaterial()
+{
+	RebuildSectionDraw();
+}
+
+void FDecalSceneProxy::RebuildSectionDraw()
+{
+	SectionDraws.clear();
+
+	if (!MeshBuffer)
+	{
+		UpdateSortKey();
+		return;
+	}
+
+	FMeshSectionDraw Draw = {};
+	Draw.FirstIndex = 0;
+	Draw.IndexCount = MeshBuffer->GetIndexBuffer().GetIndexCount();
+
+	const UDecalComponent* Comp = GetDecalComponent();
+	const FTextureResource* Texture = Comp ? Comp->GetTexture() : nullptr;
+	if (Texture)
+	{
+		Draw.DiffuseSRV = Texture->SRV;
+	}
+
+	SectionDraws.push_back(Draw);
+	UpdateSortKey();
+}
+
+UDecalComponent* FDecalSceneProxy::GetDecalComponent() const
+{
+	return static_cast<UDecalComponent*>(Owner);
+}
---
+++ KraftonEngine/Source/Engine/Render/Proxy/DecalSceneProxy.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "PrimitiveSceneProxy.h"
+
+class UDecalComponent;
+
+class FDecalSceneProxy : public FPrimitiveSceneProxy
+{
+public:
+	FDecalSceneProxy(UDecalComponent* InComponent);
+	~FDecalSceneProxy() override = default;
+
+	// FPrimitiveSceneProxy Interface
+	void UpdateTransform() override;
+	void UpdateMesh() override;
+	void UpdateMaterial() override;
+
+private:
+	void RebuildSectionDraw();
+
+private:
+	UDecalComponent* GetDecalComponent() const;
+	void RebuildSectionDraws();
+};
+
