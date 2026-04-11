625facb3394d038c100b7a2b0e76899e4e34690a
조현석 <chohs990514@gmail.com>
2026-04-11 15:23:54 +0900

    Init FireballComponent

---
+++ KraftonEngine/Source/Engine/Component/FireballComponent.cpp
@@ -0,0 +1,25 @@
+﻿#include "FireballComponent.h"
+
+void UFireballComponent::Serialize(FArchive& Ar)
+{
+
+}
+
+void UFireballComponent::PostDuplicate()
+{
+
+}
+
+void UFireballComponent::GetEditableProperties(TArray<FPropertyDescriptor>& OutProps)
+{
+
+}
+
+void UFireballComponent::PostEditProperty(const char* PropertyName)
+{
+}
+
+FPrimitiveSceneProxy* UFireballComponent::CreateSceneProxy()
+{
+	return nullptr;
+}
---
+++ KraftonEngine/Source/Engine/Component/FireballComponent.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Component/PrimitiveComponent.h"
+class UFireballComponent : public UPrimitiveComponent
+{
+	public:
+	DECLARE_CLASS(UFireballComponent, UPrimitiveComponent)
+	UFireballComponent() = default;
+	~UFireballComponent() override = default;
+	void Serialize(FArchive& Ar) override;
+	void PostDuplicate() override;
+	void GetEditableProperties(TArray<FPropertyDescriptor>& OutProps) override;
+	void PostEditProperty(const char* PropertyName) override;
+	FPrimitiveSceneProxy* CreateSceneProxy() override;
+
+	float GetIntensity() const { return Intensity; }
+	float GetRadius() const { return Radius; }
+	float GetRadiusFalloff() const { return RadiusFalloff; }
+	const FColor& GetColor() const { return Color; }
+
+
+private:
+	float Intensity = 1.0f;
+	float Radius = 100.0f;
+	float RadiusFalloff = 1.0f;
+	FColor Color;
+};
+
