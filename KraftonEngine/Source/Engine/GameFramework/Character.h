5750f73befa12694bb655eeaee72d43b38212f0a
남윤지 <rora000924@gmail.com>
2026-05-20 17:17:30 +0900

    Fix: CharacterAnimInstance UAsset Rename

--- KraftonEngine/Source/Engine/GameFramework/Character.h
+++ KraftonEngine/Source/Engine/GameFramework/Character.h
@@ -76,7 +76,7 @@ private:
 	// 시연용 state를 위한 변수들
 	// 착지 상태 관련
 	float LandingTimer = 0.0f;
-	float LandingDuration = 0.05f; // 애니메이션 길이에 맞게 조정
+	float LandingDuration = 0.02f; // 애니메이션 길이에 맞게 조정
 
 	bool bWasFallingLastFrame = false;
 
