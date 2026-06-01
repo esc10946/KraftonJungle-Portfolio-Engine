65147e3bd5ee5b445f987411e44876d1b72c39f8
kimhojun <alslehfk766@gmail.com>
2026-06-01 11:04:26 +0900

    Fix: ActorPtr inspector 버그로 인해 잠시 주석처리

--- KraftonEngine/Source/Engine/CameraDepth/CameraFocusTypes.h
+++ KraftonEngine/Source/Engine/CameraDepth/CameraFocusTypes.h
@@ -31,14 +31,14 @@ struct FCameraFocusSettings
 {
     GENERATED_BODY(FCameraFocusSettings)
 
-	UPROPERTY(Edit, Category = "Focus", DisplayName = "FocusMode")
+	//UPROPERTY(Edit, Category = "Focus", DisplayName = "FocusMode")
     // Runtime supports only manual distance in the editor for now.
     ECameraFocusMode FocusMode = ECameraFocusMode::CFM_Manual; // FocusDistance 계산 방식
 
     UPROPERTY(Edit, Category="Focus", DisplayName="Manual Focus Distance", Min=0.01f, Max=100000.0f, Speed=1.0f)
     float ManualFocusDistance = 10.0f; // 수동 초점 거리
 
-	UPROPERTY(Edit, Category = "Focus", DisplayName = "TrackingTarget", Type=SoftObject, Class=AActor)
+	//UPROPERTY(Edit, Category = "Focus", DisplayName = "TrackingTarget", Type=SoftObject, Class=AActor)
 	TSoftObjectPtr<AActor> TrackingTarget = nullptr; // 추적할 Actor
 
     float FocusInterpSpeed = 8.0f; // FocusDistance 보간 속도
