f7f4481ed3e7b755e4066c4024e9cd0f7dd9b523
alogeclock <alogeclock@gmail.com>
2026-06-02 15:11:17 +0900

    Feat: worker thread 수 최대 4개로 제한 (스레드 병목 완화)

--- KraftonEngine/Source/Engine/Physics/Backends/PhysXSceneThreading.cpp
+++ KraftonEngine/Source/Engine/Physics/Backends/PhysXSceneThreading.cpp
@@ -7,7 +7,7 @@
 uint32 GetRecommendedPhysXWorkerThreadCount()
 {
     const unsigned int LogicalCores = std::thread::hardware_concurrency();
-    return static_cast<uint32>((std::max)(1u, LogicalCores));
+    return static_cast<uint32>((std::min)(4u, (std::max)(1u, LogicalCores)));
 }
 
 // PhysX scene이 멀티스레드 solver, active actor, CCD 경로를 사용할 수 있도록 기본 flag를 설정한다.
