70ccd9a771038dbfaa68c9879ccf31f88cf168f6
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-06-04 17:57:37 +0900

    Feat: Add AFinaleGameMode header

---
+++ KraftonEngine/Source/Game/GameMode/AFinaleGameMode.cpp
@@ -0,0 +1,1 @@
+﻿#include "AFinaleGameMode.h"
---
+++ KraftonEngine/Source/Game/GameMode/AFinaleGameMode.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Engine/GameFramework/GameMode/GameModeBase.h"
+
+UCLASS()
+class AFinaleGameMode : public AGameModeBase
+{
+public:
+	AFinaleGameMode(UClass* GameStateClass, UClass* PlayerControllerClass);
+	void StartMatch() override;
+	void OnPossessedPawnEnteredTrigger(ATriggerVolumeBase* Trigger, APawn* Pawn) override;
+	void OnPossessedPawnExitedTrigger(ATriggerVolumeBase* Trigger, APawn* Pawn)  override;
+
+	// Change the scene to the target when called
+	// TODO: Change the uint8 to enum when it rolls in
+	void TransitScene(uint8 TargetScene);
+	void OnPlayerDeath();
+	void OnBossSlain(FName BossId);
+	void OnVictory();
+
+private:
+
+
+};
